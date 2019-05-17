418f719c6403cdb56f47c78ffd9fb4f9b8ecd4b7
muzin <sirius1@aliyun.com>
2019-05-17 18:08:11 +0800

    commit

--- bin/nest.js
+++ bin/nest.js
@@ -9,8 +9,8 @@ redis.psubscribe('cmd=msg:device=123:*', function (err, count) {
     console.log('start', 'err:', err, 'count:', count)
 })
 
-redis.on('pmessageBuffer', function(pattern, channel, message){
-    console.log(pattern, channel.toString(), message.toString())
+redis.on('pmessageBuffer', function(pattern, channel, data){
+    console.log(pattern, channel.toString(), data.toString())
 })
 
 
@@ -21,6 +21,8 @@ redis.on('pmessageBuffer', function(pattern, channel, message){
  *
  *      cmd 系统命令 （执行命令，获取执行命令后的信息）
  *
+ *      notification 通知
+ *
  *      sysinfo 系统信息
  *
  *      mqresp  消息队列 结果响应
@@ -28,6 +30,10 @@ redis.on('pmessageBuffer', function(pattern, channel, message){
  *      module   装配模块 （测试模块）
  *
  *      unmodule 卸载模块
+ *
+ *      cmdlist 命令列表（区分系统级，和自定义级）
+ *
+ *      自定义命令
  *
  * device 设备编号 (0为nest， 10 以下为queen， 10及以上为 ant)
  *      目标设备编号
@@ -43,6 +49,18 @@ redis.on('pmessageBuffer', function(pattern, channel, message){
  *
  *
  */
+
+
+
+/**
+ * 消息 必有 的key
+ * cmd          命令
+ * mqid         消息队列id
+ * device       目标设备
+ * from         命令来源
+ *
+ */
+
 
 
 
